3f18c46093a1a27dd3457ebba8eec97a83db2d23
zacharynsa <71812233+zacharynsa@users.noreply.github.com>
2020-10-05 16:12:39 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+   // kindly use appropriate data types for your declaration
+   //declare an array variable and assign the days of the month debtor defaulted,
+   
+   // declare variables of amount the debtor is to pay per day, and amount the debtor paid and assign values to them
+   
+   // declare all the variables needed for your calculations
+
+   // calculate and print total amount the debtor is to pay using for each loop
+
+   // calculate the days the debtor paid for and print the days in which was paid for starting from the left to the right of the array. Also check if there was a day that the debtor did not pay the complete amount for that day, if yes, print the day and the amount left to pay for that day. 
+
+   // calculate amount the debtor is left to pay if he did not pay complete fees and the days not paid for or  check if the debtor paid extra and the extra amount he paid for, or if he paid the exact amount needed to be paid and print likewise
+
+   //kindly remove the statement below when you are done with the assignment
+    System.out.println("Hello world!");
+  }
+}
